68fed5d4a0d39ba466787b547a99da98afd2dba3
Doğukan <dcatmakas@gmail.com>
2024-03-04 22:15:04 +0300

    pytest added to the project

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
+from storeapi.routers.post import router as post_router
+
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World!"}
+app.include_router(post_router)
