3047fdfcb5a82ea346e50affb3d7b28365529903
Doğukan <dcatmakas@gmail.com>
2024-03-06 15:07:47 +0300

    pytest updates

---
+++ storeapi/tests/routers/test_post.py
@@ -0,0 +1,37 @@
+import pytest
+from httpx import AsyncClient
+
+
+async def create_post(body: str, async_client: AsyncClient) -> dict:
+    response = await async_client.post("/post", json={"body": body})
+    return response.json()
+
+
+@pytest.fixture()
+async def created_post(async_client: AsyncClient):
+    return await create_post("Test Post", async_client)
+
+
+@pytest.mark.asyncio
+async def test_create_post(async_client: AsyncClient):
+    body = "Test Post"
+
+    response = await async_client.post("/post", json={"body": body})
+
+    assert response.status_code == 201
+    assert {"id": 0, "body": body}.items() <= response.json().items()
+
+
+@pytest.mark.asyncio
+async def test_create_post_missing_data(async_client: AsyncClient):
+    response = await async_client.post("/post", json={})
+
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_get_all_posts(async_client: AsyncClient, created_post: dict):
+    response = await async_client.get("/post")
+
+    assert response.status_code == 200
+    assert response.json() == [created_post]
